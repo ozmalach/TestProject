b1572836d0c0927eef2f9eb92c156fb77983b1a9
ozmalach <59977953+ozmalach@users.noreply.github.com>
2020-05-14 12:06:41 +0300

    Add files via upload

--- BlockPazzle.js
+++ BlockPazzle.js
@@ -1,3 +1,23 @@
 function refreshPage(){
     window.location.reload();
-} 
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+  
+function drag(ev) {
+    ev.dataTransfer.setData("transferedObj", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("transferedObj");
+    console.log(ev);
+    console.log(ev.dataTransfer);
+    console.log(data);
+    console.log(ev.target);
+    // ev.target.style.background = rgb(172, 11, 221);
+    ev.target.appendChild(document.getElementById(data));
+    // console.log(ev.target)
+}
